85a00775e7cac6df57f6feea320029e4658e4992
valeriogonini <valeriogonini@gmail.com>
2024-05-29 14:37:25 +0200

    add comics.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $comics = config('comics');
+
+    dump($comics);
+    return view('welcome', compact('comics'));
 });
 
 
