598c5ceb595713e7f6b6d81831c6599a6c05de86
valeriogonini <valeriogonini@gmail.com>
2024-05-29 16:50:37 +0200

    add show.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,5 +2,21 @@
 
 
 @section('content')
-    <h1>ciao</h1>
+    <div class="container">
+      <div class="row">
+    @foreach ($comics as $comic)
+     <div class="col-4 gy-3">
+        <div class="card h-100" style="width: 18rem;">
+            <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{$comic->title}}</h5>
+
+            <a href="{{ route('comics.show', $comic) }}" class="btn btn-primary">Descrizione</a>
+            </div>
+          </div>
+    </div>
+    @endforeach
+
+     </div>
+    </div>
 @endsection
--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,5 +3,24 @@
 
 
 @section('content')
-    
+<div class="container">
+    <div class="row">
+   <div class="col-4 gy-3">
+      <div class="card h-100" style="width: 18rem;">
+          <img src="{{$comic->thumb}}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{$comic->title}}</h5>
+            <p class="card-text">{{$comic->description}}</p>
+            <p class="card-text">{{$comic->price}}</p>
+            <p class="card-text">{{$comic->series}}</p>
+            <p class="card-text">{{$comic->sale_date}}</p>
+            <p class="card-text">{{$comic->type}}</p>
+
+
+          </div>
+        </div>
+  </div>
+
+   </div>
+  </div>
 @endsection
